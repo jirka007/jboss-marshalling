5aaf519bc67e2d3cb92e2555924c3bba19aa2e24
Jiri Bilek <jbilek@redhat.com>
2018-03-16 16:19:57 +0100

    .

--- tests/src/test/java/org/jboss/test/marshalling/OtherMarshallerTests.java
+++ tests/src/test/java/org/jboss/test/marshalling/OtherMarshallerTests.java
@@ -18,14 +18,22 @@
 
 package org.jboss.test.marshalling;
 
-import java.io.Serializable;
-import java.util.concurrent.PriorityBlockingQueue;
-import org.jboss.marshalling.cloner.ClonerConfiguration;
+import org.jboss.marshalling.Marshaller;
+import org.jboss.marshalling.MarshallerFactory;
+import org.jboss.marshalling.Marshalling;
 import org.jboss.marshalling.MarshallingConfiguration;
+import org.jboss.marshalling.Unmarshaller;
+import org.jboss.marshalling.cloner.ClonerConfiguration;
 import org.jboss.marshalling.cloner.ObjectCloner;
 import org.jboss.marshalling.cloner.ObjectClonerFactory;
 import org.jboss.marshalling.cloner.ObjectCloners;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.concurrent.PriorityBlockingQueue;
 
 
 /**
@@ -35,7 +43,7 @@ public final class OtherMarshallerTests extends TestBase {
 
     public OtherMarshallerTests(TestMarshallerProvider testMarshallerProvider, TestUnmarshallerProvider testUnmarshallerProvider, MarshallingConfiguration configuration) {
         super(testMarshallerProvider, testUnmarshallerProvider, configuration);
-    }    
+    }
 
     @Test
     public synchronized void testPriorityBlockingQueue() throws Throwable {
@@ -55,4 +63,88 @@ public final class OtherMarshallerTests extends TestBase {
             this.queue.add(item);
         }
     }
+
+    @Test
+    public void main(String[] args) throws Exception {
+        C1 object = new C1();
+        write(object, "marshall.ser");
+        C1 object2 = (C1) read("marshall.ser");
+
+        // this was cloning without issue, so the bug appears to only be when marshalling above
+        C1 objectClone = (C1) clone(object);
+    }
+
+    public void write(Object object, String file) throws Exception {
+
+        // Get the factory for the "river" marshalling protocol
+        final MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("river");
+
+        // Create a configuration
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        // Use version 3
+        //configuration.setVersion(3);
+        final Marshaller marshaller = marshallerFactory.createMarshaller(configuration);
+        final FileOutputStream os = new FileOutputStream(file);
+        try {
+            marshaller.start(Marshalling.createByteOutput(os));
+            marshaller.writeObject(object);
+            marshaller.finish();
+            os.close();
+        } finally {
+            // clean up stream resource
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Object read(String file) throws Exception {
+        // Get the factory for the "river" marshalling protocol
+        final MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("river");
+
+        // Create a configuration
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        Object object = null;
+        final Unmarshaller unmarshaller = marshallerFactory.createUnmarshaller(configuration);
+        final FileInputStream is = new FileInputStream(file);
+        try {
+            unmarshaller.start(Marshalling.createByteInput(is));
+            object =  unmarshaller.readObject();
+            unmarshaller.finish();
+            is.close();
+        } finally {
+            // clean up stream resource
+            try {
+                is.close();
+            } catch (IOException e) {
+                System.err.print("Stream close failed: ");
+                e.printStackTrace();
+            }
+        }
+        return object;
+    }
+
+    public Object clone(Object object) throws Exception {
+        final ObjectClonerFactory clonerFactory = ObjectCloners.getSerializingObjectClonerFactory();
+        final ClonerConfiguration configuration = new ClonerConfiguration();
+        final ObjectCloner cloner = clonerFactory.createCloner(configuration);
+        return cloner.clone(object);
+    }
+
+    public class C1 implements Serializable {
+        C2 x, y;
+        public C1() {
+            x = new C2();
+            y = x;
+        }
+    }
+
+    public class C2 implements Serializable {
+        Object readResolve() {
+            return null;
+        }
+    }
+
 }
